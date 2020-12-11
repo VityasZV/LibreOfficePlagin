45af7da4a1ea6b6e7ec2179a9e1ee6be5b1ff0dc
VityasZV <viktor.zadyabin@mail.ru>
2020-12-11 11:26:55 +0300

    done

--- FunctionsTask/Google_tests/ExponentialTests.cpp
+++ FunctionsTask/Google_tests/ExponentialTests.cpp
@@ -6,15 +6,12 @@
 
 class ExponentTests : public ::testing::Test {
 protected:
-    std::shared_ptr<BaseFunc> a, b, c, d, e, g;
+    std::shared_ptr<BaseFunc> a, b, c;
     void SetUp() override {
         FuncFactory factory;
         a = factory.create("exp", 0);
         b = factory.create("exp", {0, 1});
         c = factory.create("exp", {-1, -1, 2, -2, 0, 1});
-        d = factory.create("exp", {1, 0, 1});
-        e = factory.create("exp", 1.456);
-        g = factory.create("exp");
     }
 };
 
@@ -22,10 +19,7 @@ protected:
 TEST_F(ExponentTests, ToStringTest) {
     EXPECT_STREQ(a->ToString().c_str(), "1");
     EXPECT_STREQ(b->ToString().c_str(), "1");
-    EXPECT_STREQ(c->ToString().c_str(), "exp(-x)");
-    EXPECT_STREQ(d->ToString().c_str(), "exp(x)");
-    EXPECT_STREQ(e->ToString().c_str(), "exp(1.456x)");
-    EXPECT_STREQ(g->ToString().c_str(), "1");
+    EXPECT_STREQ(c->ToString().c_str(), "exp(-1.000000x)");
 }
 
 TEST_F(ExponentTests, OperatorParenthesesTest) {
@@ -33,8 +27,4 @@ TEST_F(ExponentTests, OperatorParenthesesTest) {
     EXPECT_DOUBLE_EQ((*a)(-12), 1);
     EXPECT_NEAR((*c)(-12), exp(12), 0.01);
     EXPECT_NEAR((*c)(12), exp(-12), 0.01);
-    EXPECT_NEAR((*d)(-12), exp(-12), 0.01);
-    EXPECT_NEAR((*d)(12), exp(12), 0.01);
-    EXPECT_NEAR((*e)(-1), exp(-1.456), 0.01);
-    EXPECT_NEAR((*e)(1), exp(1.456), 0.01);
 }
--- FunctionsTask/Google_tests/PolynomialTests.cpp
+++ FunctionsTask/Google_tests/PolynomialTests.cpp
@@ -4,17 +4,15 @@
 
 class PolynomialTests: public ::testing::Test {
 public:
-    std::shared_ptr<BaseFunc> a, b, c, d, e;
+    std::shared_ptr<BaseFunc> c, d, e;
     void SetUp( ) {
         FuncFactory factory;
-        a = factory.create("polynomial", 0);
-        b = factory.create("polynomial", {0, 1});
-        c = factory.create("polynomial", {-1, -1, 2, -2, 0, 1});
+        c = factory.create("polynomial", {-1, -1, 3, -4, 0, 1});
         d = factory.create("ident", {1, 0, 1});
         e = factory.create("const", 1.456);
     }
 
-    void TearDown( ) {
+    void TearDown() {
         // code to run after each test;
         // can be used instead of a destructor,
         // but exceptions can be handled in this function only
@@ -23,33 +21,19 @@ public:
 };
 
 TEST_F(PolynomialTests, ToStringTest) {
-    EXPECT_STREQ(a->ToString().c_str(), "0");
-    EXPECT_STREQ(b->ToString().c_str(), "x");
-    EXPECT_STREQ(c->ToString().c_str(), "-1-x+2x^2-2x^3+x^5");
-    EXPECT_STREQ(d->ToString().c_str(), "x");
-    EXPECT_STREQ(e->ToString().c_str(), "1.456");
+    EXPECT_STREQ(c->ToString().c_str(), "-1-x+3x^2-4x^3+x^5");
+    EXPECT_STREQ(d->ToString().c_str(), "+x");
+    EXPECT_STREQ(e->ToString().c_str(), "+1.456");
 }
 
 TEST_F(PolynomialTests, OperatorParenthesesTest) {
-    EXPECT_DOUBLE_EQ((*a)(1), 0);
-    EXPECT_DOUBLE_EQ((*a)(123), 0);
-    EXPECT_DOUBLE_EQ((*b)(1), 1);
-    EXPECT_DOUBLE_EQ((*b)(123), 123);
-    EXPECT_DOUBLE_EQ((*c)(1), -1);
-    EXPECT_DOUBLE_EQ((*c)(0), -1);
-    EXPECT_DOUBLE_EQ((*c)(-1), 3);
-    EXPECT_DOUBLE_EQ((*d)(-1), -1);
+    EXPECT_DOUBLE_EQ((*c)(1), -2);
     EXPECT_DOUBLE_EQ((*d)(-123), -123);
-    EXPECT_DOUBLE_EQ((*e)(-1), 1.456);
     EXPECT_DOUBLE_EQ((*e)(-123), 1.456);
 }
 
 TEST_F(PolynomialTests, DerivativesTest) {
-    EXPECT_DOUBLE_EQ(a->GetDerivativeAtPoint(0), 0);
-    EXPECT_DOUBLE_EQ(a->GetDerivativeAtPoint(34), 0);
-    EXPECT_DOUBLE_EQ(b->GetDerivativeAtPoint(34), 1);
-    EXPECT_DOUBLE_EQ(b->GetDerivativeAtPoint(34), 1);
-    EXPECT_DOUBLE_EQ(c->GetDerivativeAtPoint(1), 2);
+    EXPECT_DOUBLE_EQ(c->GetDerivativeAtPoint(1), -2);
     EXPECT_DOUBLE_EQ(c->GetDerivativeAtPoint(0), -1);
-    EXPECT_DOUBLE_EQ(c->GetDerivativeAtPoint(-1), -6);
+    EXPECT_DOUBLE_EQ(c->GetDerivativeAtPoint(-1), -14);
 }
